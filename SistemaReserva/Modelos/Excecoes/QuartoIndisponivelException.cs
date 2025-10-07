b49f31d11493ca206d278e4ad80079abc41acf96
MuriloF <murilofs.franca@gmail.com>
2025-10-07 00:23:54 -0300

    feat: Criado quarto indisponivel e reserva invalida exceptions, criado a classe quarto

---
+++ SistemaReserva/Modelos/Excecoes/QuartoIndisponivelException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReserva.Modelos.Excecoes
+{
+    public class QuartoIndisponivelException : Exception
+    {
+        public QuartoIndisponivelException(string message) : base(message) { }
+    }
+}
---
+++ SistemaReserva/Modelos/Excecoes/ReservaInvalidaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaReserva.Modelos.Excecoes
+{
+    public class ReservaInvalidaException : Exception
+    {
+        public ReservaInvalidaException(string mensagem) : base(mensagem) { }
+           
+        
+        
+    }
+}
