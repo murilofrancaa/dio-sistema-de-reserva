944204191ad038d36aadfc7bd4df1379421bc0db
MuriloF <murilofs.franca@gmail.com>
2025-10-07 00:26:26 -0300

    feat: finalizado a criação da classe Quarto

--- SistemaReserva/Modelos/Quarto.cs
+++ SistemaReserva/Modelos/Quarto.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using SistemaReserva.Modelos.Excecoes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace SistemaReserva.Modelos
 {
-    internal class Quarto
+    public class Quarto
     {
+
+        public int NumeroQuarto { get; private set; }
+
+        public string TipoQuarto { get; private set; }
+
+        public decimal PrecoPorNoite { get; private set; }
+
+        public bool Disponivel { get; private set; }
+
+        public Quarto(int numero, string tipo, decimal preco, bool disponivel = true)
+        {
+            NumeroQuarto = numero;
+            TipoQuarto = tipo;
+            PrecoPorNoite = preco;
+            Disponivel = disponivel;
+
+            if (preco <= 0) { throw new ArgumentException("O preço da diária deve ser maior que zero. " +
+               "Informe um valor válido para o quarto."); }
+        }
+
+        public void Ocupar()
+        {
+            if (!Disponivel)
+            {
+                throw new QuartoIndisponivelException("O quarto já está ocupado.");
+
+            }
+
+            Disponivel = false;
+
+        }
+
+        public void Liberar()
+        {
+            Disponivel = true;
+
+            Console.WriteLine($"Quarto {NumeroQuarto} liberado com sucesso!");
+        }
+
+        public override string ToString()
+        {
+
+            string status = Disponivel ? "Sim" : "Não";
+            return $"Quarto {NumeroQuarto} - Tipo: {TipoQuarto} | R${PrecoPorNoite} por noite | Disponível: {status}";
+        }
     }
 }
+
+     
+
